acc8136a8cd35489c5a6ab5cd5ae80e6e47d61a8
Debayan <pradhandebayan@gmail.com>
2022-03-02 18:08:46 +0530

    added screen recorder feature

--- code.py
+++ code.py
@@ -10,6 +10,9 @@ import os
 import psutil
 from bs4 import BeautifulSoup
 import requests
+import cv2
+import pyautogui
+import numpy as np
 
 
 
@@ -57,6 +60,36 @@ def take_command():
         pass
     return command
 
+
+def screen_record():                                                                   #Screen Recording function              
+                                                                                       
+    user = psutil.users()[0].name                                                      #gets the user name 
+    f_path = "C:\\Users\\" + user + "\\Desktop"                                        
+    # f_path = input("Enter where u want to save the file")                            #can also have an option as to where the user wants to save the file
+    save_file = os.path.join(f_path,"output.avi")                                      #saves the file in the corresponding directory
+
+    s_size = (1920,1080)
+    fps = 10.0                                                                         #determines how fast the slow the playback speed will be
+
+    fcc = cv2.VideoWriter_fourcc(*"mp4v")
+    out = cv2.VideoWriter("output.avi",fcc,fps,(s_size))
+
+    while 1:
+
+        img = pyautogui.screenshot()
+        frm = np.array(img)
+        frm = cv2.cvtColor(frm,cv2.COLOR_BGR2RGB)
+        cv2.imshow("recorder",frm)
+        cv2.namedWindow("recorder",cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("recorder", 800,800)
+        out.write(frm)
+        
+        key = cv2.waitKey(1)                                                             # q to close the window and stop recording
+        if key == 113:
+            break
+
+    cv2.destroyAllWindows()
+    out.release()
 
 
 
@@ -133,6 +166,11 @@ def run_kavi():
         weather(city)
         abc = ("Have a Nice Day Buddy")
         talk(abc)
+
+    elif 'screen recording' in command:                                # Screen Recroding command                         
+        talk('press q to stop and save recording')
+        print('press q to stop and save recording')
+        screen_record() 
 
     elif 'movie review' in command:
         movie = command.replace('movie review', '')
