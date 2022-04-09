0e9725dd64b1b2f49b5bfcc6559295bb06a813f1
KUSHAGRA DUBEY <76646470+TheCreaToR-01@users.noreply.github.com>
2022-04-09 08:11:21 -0700

    Adding some fun question to Kavi

--- modules.py
+++ modules.py
@@ -5,13 +5,15 @@ from PIL import Image
 import wikipedia
 import pywhatkit
 import pyttsx3
+import speech_recognition as sr
 import webbrowser
 import requests
 import psutil
 import datetime
 import random
 import time
-import speech_recognition as sr
+import pygame
+from pygame import mixer
 
 
 
@@ -19,48 +21,35 @@ import speech_recognition as sr
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
+listener = sr.Recognizer()
+mixer.init()
+
+def take_command():
+    '''
+    This function takes users's voice command and recognizes them.
+    '''
+
+    r = sr.Recognizer()
+    # try taking in command
+    while True: 
+        with sr.Microphone() as source:
+            print("Listening...")
+            talk("listening.....")
+            audio = r.record(source, duration=3)
+
+            try:
+                command = r.recognize_google(audio, language='en-in')
+                print(f"user said:{command}\n")
+                return command
+
+            except Exception as e:
+                # ask user to repeat if there was some error getting/recognizing it clearly 
+                talk("Pardon me,Please say that again")
+                print("Pardon me,Please say that again")    
+    return 
 
 
 
-
-#to do list
-def take_notes():
-    r5 = sr.Recognizer()  
-    with sr.Microphone() as source:
-        print('What is your "TO DO LIST" for today')
-        engine.say('What is your "TO DO LIST" for today')
-        engine.runAndWait()
-        audio = r5.listen(source)
-        audio = r5.recognize_google(audio)
-        print(audio)
-       # today = date.today()
-       # today = str(today)
-        with open("kavi.txt","a") as f:
-            #f.write('\n')
-            #f.write(today)
-            f.write('\n')
-            f.write(audio)
-            f.write('\n')
-            f.write(audio)
-            f.write('\n')
-            f.write(audio)
-            f.write('\n')
-            f.write(audio)
-            f.write('\n')
-            #f.write('......')
-            #f.write('\n')
-            f.close() 
-        engine.say('Notes Taken')
-        engine.runAndWait()
-
-def show_notes():
-    with open("kavi.txt", "r") as f:
-        task = f.read()
-        task = task.split('......')
-    engine.say(task[-2])
-    engine.runAndWait()
-
-# main talk function, that will be used to provide voice output to the user
 
 def talk(text=None, ques=None):
 
@@ -77,34 +66,39 @@ def talk(text=None, ques=None):
             if 'are you single' in ques :
                 engine.say('I am in relationship with wireless devices')
             elif 'do you like me' in ques:
-                engine.say('yes boss definitely')
+                engine.say("I don't have any reason to not like you.")
             elif 'what is your name' in ques:
                 engine.say('My devloper karunakran has named me kkavi')
-            elif 'cringe' in ques:
-                engine.say('alright........he/she was funniest person')
             elif 'joke' in ques:
                 joke = get_joke()
                 print(joke)
-
-                engine.say(joke)
-                   
-
                 engine.say(joke)        
-
             elif 'i am tired' in ques:
                 engine.say('you should take a break')
             elif 'favorite game' in ques:
                 engine.say('my favorite game is chess')
             elif 'can you dance' in ques:
-                engine.say('I cant dance as of now, but I can play some dance music')
+                engine.say("I can't dance as of now, but I can play some dance music")
             elif 'how do i look' in ques:
                 engine.say('juding from your voice, amazing')
             elif 'can you cook' in ques:
                 engine.say('i can cook you up amazing bedtime stories if you want')
             elif 'who is your friend' in ques:
                 engine.say('her name is nilla voice assistant, she is in another repository')
+            elif 'who is alexa' in ques:
+                engine.say('Alexa is my idol. I always learns a lot from her.')
+            elif 'who is your favourite player' in ques:
+                engine.say('Mine is obvoiusly you, tell me yours.')
+            elif 'who is google assitant' in ques:
+                engine.say('She is like my sister.')
+            elif 'who is siri' in ques:
+                engine.say('She is my cousin')
+            elif 'where do you live' in ques:
+                engine.say('I lives in Kavi Assitant github repository.')
+            elif 'do you eat' in ques:
+                engine.say('Not yet, but planning to the bugs who want to stop me.')
             else:
-                engine.say("Sorry, couldn't get you :( ")
+                engine.say("Sorry, couldn't get you")
         
     else :
         # vocalize text output 
@@ -415,21 +409,11 @@ def get_joke():
 
 
 
-
-
-
-
-
-
-
-
-
 def intro():
 
     '''
 
     This function introduces Kavi to users.
-
 
     '''
 
@@ -440,3 +424,52 @@ def intro():
     talk("")
     return 
 
+def fly_away_bird_game(ctx):
+      '''
+      This function plays fly away bird game with the user
+
+      '''
+
+      fly_away_bird_dict = {'dog':'pass', 'snake':'pass', 'bee':'fly', 'dragonfly':'fly', 'sparrow':'fly', 'ostrich':'pass', 'lizard':'pass', 'eagle':'fly', 'crow':'fly', 'parrot':'fly', 'tortoise':'pass', 'squirrel':'pass', 'shark':'pass', 'penguin':'pass', 'bat':'fly', 'vulture':'fly', 'earthworm':'pass', 'frog':'pass', 'swan':'fly', 'butterfly':'fly', 'peacock':'fly', 'owl':'fly'}   #assistant uses this to play with user
+
+      #Instructions of the game
+      talk('Welcome to the Fly Away Bird Game. Just say fly if flies and pass if not.')
+      print('Welcome to the Fly Away Bird Game. Just say fly if flies and pass if not.')
+      mixer.music.load('./start.mp3')
+      mixer.music.play()
+      pygame.time.wait(1)
+
+      #local variables for game to count
+      game_round_count = 0
+      user_game_score = 0 
+
+      while game_round_count < 15:
+            assistant_choice = random.choice(list(fly_away_bird_dict.keys()))
+            talk(f"{assistant_choice} fly")
+            game_round_count +=1
+            user_choice = str(take_command()).lower()
+            if user_choice == str(fly_away_bird_dict.get(assistant_choice)):
+                  mixer.music.load('./correct.mp3')
+                  mixer.music.play()
+                  user_game_score +=1
+                  pygame.time.wait(2)
+            elif user_choice != str(fly_away_bird_dict.get(assistant_choice)) and user_choice == 'pass' or user_choice == 'fly':
+                  mixer.music.load('./wrong.mp3')
+                  mixer.music.play()
+                  pygame.time.wait(2)
+                  talk("I don't think so.")
+            else:
+                  print("Could not recognized")
+
+      #scorecard of game
+      if user_game_score >= 12:
+            mixer.music.load('./celebration.mp3') # plays celebration song
+            talk(f"You are a champ. You got {user_game_score} out of 15 rounds correct.")
+            mixer.music.play()
+            print(f"Score: {user_game_score}")
+      elif user_game_score >= 8:
+            talk(f'Well played. You got {user_game_score} out of 15 rounds correct.')
+            print(f"Score: {user_game_score}")
+      else:
+            talk(f'Try harder, you will be best. You got {user_game_score} out of 15 rounds correct.')
+            print(f"Score: {user_game_score}")
