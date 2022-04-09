f8b3eef4c6223a12b51255cfc5cba00a14be6dbd
KUSHAGRA DUBEY <76646470+TheCreaToR-01@users.noreply.github.com>
2022-04-09 02:40:03 -0700

    Fly Bird Game

--- modules.py
+++ modules.py
@@ -5,13 +5,15 @@ from PIL import Image
 import wikipedia
 import pywhatkit
 import pyttsx3
+import speech_recognition as sr
 import webbrowser
 import requests
 import psutil
 import datetime
 import random
 import time
-import speech_recognition as sr
+import pygame
+from pygame import mixer
 
 
 
@@ -19,48 +21,35 @@ import speech_recognition as sr
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
+listener = sr.Recognizer()
+mixer.init()
+
+def take_command():
+    '''
+    This function takes users's voice command and recognizes them.
+    '''
+
+    r = sr.Recognizer()
+    # try taking in command
+    while True: 
+        with sr.Microphone() as source:
+            print("Listening...")
+            talk("listening.....")
+            audio = r.record(source, duration=3)
+
+            try:
+                command = r.recognize_google(audio, language='en-in')
+                print(f"user said:{command}\n")
+                return command
+
+            except Exception as e:
+                # ask user to repeat if there was some error getting/recognizing it clearly 
+                talk("Pardon me,Please say that again")
+                print("Pardon me,Please say that again")    
+    return 
 
 
 
-
-#to do list
-def take_notes():
-    r5 = sr.Recognizer()  
-    with sr.Microphone() as source:
-        print('What is your "TO DO LIST" for today')
-        engine.say('What is your "TO DO LIST" for today')
-        engine.runAndWait()
-        audio = r5.listen(source)
-        audio = r5.recognize_google(audio)
-        print(audio)
-       # today = date.today()
-       # today = str(today)
-        with open("kavi.txt","a") as f:
-            #f.write('\n')
-            #f.write(today)
-            f.write('\n')
-            f.write(audio)
-            f.write('\n')
-            f.write(audio)
-            f.write('\n')
-            f.write(audio)
-            f.write('\n')
-            f.write(audio)
-            f.write('\n')
-            #f.write('......')
-            #f.write('\n')
-            f.close() 
-        engine.say('Notes Taken')
-        engine.runAndWait()
-
-def show_notes():
-    with open("kavi.txt", "r") as f:
-        task = f.read()
-        task = task.split('......')
-    engine.say(task[-2])
-    engine.runAndWait()
-
-# main talk function, that will be used to provide voice output to the user
 
 def talk(text=None, ques=None):
 
@@ -85,12 +74,7 @@ def talk(text=None, ques=None):
             elif 'joke' in ques:
                 joke = get_joke()
                 print(joke)
-
-                engine.say(joke)
-                   
-
                 engine.say(joke)        
-
             elif 'i am tired' in ques:
                 engine.say('you should take a break')
             elif 'favorite game' in ques:
@@ -415,21 +399,11 @@ def get_joke():
 
 
 
-
-
-
-
-
-
-
-
-
 def intro():
 
     '''
 
     This function introduces Kavi to users.
-
 
     '''
 
@@ -440,3 +414,52 @@ def intro():
     talk("")
     return 
 
+def fly_away_bird_game(ctx):
+      '''
+      This function plays fly away bird game with the user
+
+      '''
+
+      fly_away_bird_dict = {'dog':'pass', 'snake':'pass', 'bee':'fly', 'dragonfly':'fly', 'sparrow':'fly', 'ostrich':'pass', 'lizard':'pass', 'eagle':'fly', 'crow':'fly', 'parrot':'fly', 'tortoise':'pass', 'squirrel':'pass', 'shark':'pass', 'penguin':'pass', 'bat':'fly', 'vulture':'fly', 'earthworm':'pass', 'frog':'pass', 'swan':'fly', 'butterfly':'fly', 'peacock':'fly', 'owl':'fly'}   #assistant uses this to play with user
+
+      #Instructions of the game
+      talk('Welcome to the Fly Away Bird Game. Just say fly if flies and pass if not.')
+      print('Welcome to the Fly Away Bird Game. Just say fly if flies and pass if not.')
+      mixer.music.load('./start.mp3')
+      mixer.music.play()
+      pygame.time.wait(1)
+
+      #local variables for game to count
+      game_round_count = 0
+      user_game_score = 0 
+
+      while game_round_count < 15:
+            assistant_choice = random.choice(list(fly_away_bird_dict.keys()))
+            talk(f"{assistant_choice} fly")
+            game_round_count +=1
+            user_choice = str(take_command()).lower()
+            if user_choice == str(fly_away_bird_dict.get(assistant_choice)):
+                  mixer.music.load('./correct.mp3')
+                  mixer.music.play()
+                  user_game_score +=1
+                  pygame.time.wait(2)
+            elif user_choice != str(fly_away_bird_dict.get(assistant_choice)) and user_choice == 'pass' or user_choice == 'fly':
+                  mixer.music.load('./wrong.mp3')
+                  mixer.music.play()
+                  pygame.time.wait(2)
+                  talk("I don't think so.")
+            else:
+                  print("Could not recognized")
+
+      #scorecard of game
+      if user_game_score >= 12:
+            mixer.music.load('./celebration.mp3') # plays celebration song
+            talk(f"You are a champ. You got {user_game_score} out of 15 rounds correct.")
+            mixer.music.play()
+            print(f"Score: {user_game_score}")
+      elif user_game_score >= 8:
+            talk(f'Well played. You got {user_game_score} out of 15 rounds correct.')
+            print(f"Score: {user_game_score}")
+      else:
+            talk(f'Try harder, you will be best. You got {user_game_score} out of 15 rounds correct.')
+            print(f"Score: {user_game_score}")
